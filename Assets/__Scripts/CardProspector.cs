ceac3c0931ea1aabe9b3e894011962ae092073af
bursalma <32002049+bursalma@users.noreply.github.com>
2017-12-01 19:59:02 -0500

    Page 593

---
+++ Assets/__Scripts/CardProspector.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum CardState
+{
+    drawpile,
+    tableu,
+    target,
+    discard
+}
+
+public class CardProspector : Card {
+
+    public CardState state = CardState.drawpile;
+    public List<CardProspector> hiddenBy = new List<CardProspector>();
+    public int layoutID;
+    public SlotDef slotDef;
+
+}
